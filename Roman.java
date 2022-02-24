00235d74aeb33fa0b86af8a4b0aa580588ba4504
Juwon <juwon8@vt.edu>
2022-02-24 11:24:57 -0500

    full romToInt implementation

--- Roman.java
+++ Roman.java
@@ -1,20 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Roman{
 	public static void main (String[] args){
-		// static Map<String, Integer> values = new HashMap<>();
+		System.out.println(romanToInt("I"));
 
-		// static {
-		// 	values.put("M",1000);
-		// 	values.put("D", 500);
-		// 	values.put("C", 100);
-		// 	values.put("L", 50);
-		// 	values.put("X", 10);
-		// 	values.put("V", 5);
-		// 	values.put("I", 1);
-		// }
 	}
 
-	public int romanToInt(String s){
-		return 1;
+	static Map<String, Integer> values = new HashMap<>();
+    
+    static {
+        values.put("M", 1000);
+        values.put("D", 500);
+        values.put("C", 100);
+        values.put("L", 50);
+        values.put("X", 10);
+        values.put("V", 5);
+        values.put("I", 1);
+    }
+	public static int romanToInt(String s){
+		int sum = 0;
+        int i = 0;
+        while (i < s.length()) {
+            String currentSymbol = s.substring(i, i + 1);
+            int currentValue = values.get(currentSymbol);
+            int nextValue = 0;
+            if (i + 1 < s.length()) {
+                String nextSymbol = s.substring(i + 1, i + 2);
+                nextValue = values.get(nextSymbol);
+            }
+            
+            if (currentValue < nextValue) {
+                sum += (nextValue - currentValue);
+                i += 2;
+            }
+            else {
+                sum += currentValue;
+                i += 1;
+            }
+            
+        }
+        return sum;
 	}
 
 }
