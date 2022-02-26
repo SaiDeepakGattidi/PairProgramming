de45b6d896685d1a822400338ad3151aef75a11c
SAI DEEPAK GATTIDI <49058627+SaiDeepakGattidi@users.noreply.github.com>
2022-02-25 19:42:48 -0500

    Update Roman.java

--- Roman.java
+++ Roman.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Roman {
@@ -28,51 +27,3 @@ public class Roman {
         System.out.println(sb.toString());
     }
 }
-=======
-import java.util.HashMap;
-import java.util.Map;
-
-class Roman{
-	public static void main (String[] args){
-		System.out.println(romanToInt("I"));
-
-	}
-
-	static Map<String, Integer> values = new HashMap<>();
-    
-    static {
-        values.put("M", 1000);
-        values.put("D", 500);
-        values.put("C", 100);
-        values.put("L", 50);
-        values.put("X", 10);
-        values.put("V", 5);
-        values.put("I", 1);
-    }
-	public static int romanToInt(String s){
-		int sum = 0;
-        int i = 0;
-        while (i < s.length()) {
-            String currentSymbol = s.substring(i, i + 1);
-            int currentValue = values.get(currentSymbol);
-            int nextValue = 0;
-            if (i + 1 < s.length()) {
-                String nextSymbol = s.substring(i + 1, i + 2);
-                nextValue = values.get(nextSymbol);
-            }
-            
-            if (currentValue < nextValue) {
-                sum += (nextValue - currentValue);
-                i += 2;
-            }
-            else {
-                sum += currentValue;
-                i += 1;
-            }
-            
-        }
-        return sum;
-	}
-
-}
->>>>>>> main
